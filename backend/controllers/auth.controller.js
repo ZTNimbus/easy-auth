530865980d5a9efae1d0b233add5d38d1480cc03
nimbus <zerotonimbus@gmail.com>
2025-03-04 11:31:01 +0300

    implement signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,46 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateVerificationToken from "../utils/generateVerificationToken.js";
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
+
 export async function signup(req, res) {
-  res.send("signup");
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required.");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists.");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const verificationToken = generateVerificationToken();
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 Hours
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      data: { ...user._doc, password: null },
+      message: "User created.",
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
 }
 
 export async function login(req, res) {
